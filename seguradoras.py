4a82c804bc1df279c4211c273b2dbf80c6b1404b
DavidMacedodev <davidmacedo.dev@gmail.com>
2026-01-11 09:09:13 -0300

    ChatBot AguiaSeguros FastApi

---
+++ seguradoras.py
@@ -0,0 +1,13 @@
+SEGURADORAS = {
+    "1": {"nome": "BRADESCO SEGUROS", "telefone": "4004 2757"},
+    "2": {"nome": "TOKIO MARINE", "telefone": "0800 31 86546"},
+    "3": {"nome": "PORTO SEGURO", "telefone": "0800 727 0800"},
+    "4": {"nome": "ITAU SEGUROS", "telefone": "0800 720 1010"},
+    "5": {"nome": "YELUM/LIBERTY", "telefone": "0800 701 4120"},
+    "6": {"nome": "MAPFRE SEGUROS", "telefone": "0800 775 4545"},
+    "7":  {"nome": "AZUL SEGUROS", "telefone": "4004-3700"},
+    "8":  {"nome": "ZURICH SEGUROS", "telefone": "0800 285 4141"},
+    "9":  {"nome": "ALLIANZ SEGUROS", "telefone": "0800 0130 700"},
+    "10":  {"nome": "HDI SEGUROS", "telefone": "3003-5390"},
+    "11":  {"nome": "SUHAI SEGURADORA", "telefone": "0800-327-8424"}
+}
